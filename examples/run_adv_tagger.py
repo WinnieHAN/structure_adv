e98e5575048b03e8b9e3905676f7c3764f07c32c
ehaschia <guiwuqiansha@gmail.com>
2020-05-17 18:24:01 +0800

    param fix

--- examples/run_adv_tagger.py
+++ examples/run_adv_tagger.py
@@ -68,8 +68,8 @@ def main():
     parser.add_argument('--rl_finetune_seq2seq_save_path', default='tagging_models/tagging/rl_finetune/seq2seq_save_model', type=str, help='rl_finetune_seq2seq_save_path')
     parser.add_argument('--rl_finetune_network_save_path', default='tagging_models/tagging/rl_finetune/network_save_model', type=str, help='rl_finetune_network_save_path')
 
-    parser.add_argument('--rl_finetune_seq2seq_load_path', default='/media/zhanglw/2EF0EF5BF0EF27B3/code/rl_finetune/seq2seq_save_model', type=str, help='rl_finetune_seq2seq_load_path')
-    parser.add_argument('--rl_finetune_network_load_path', default='/media/zhanglw/2EF0EF5BF0EF27B3/code/rl_finetune/network_save_model', type=str, help='rl_finetune_network_load_path')
+    parser.add_argument('--rl_finetune_seq2seq_load_path', default='tagging_models/tagging/rl_finetune/network_save_model', type=str, help='rl_finetune_seq2seq_load_path')
+    parser.add_argument('--rl_finetune_network_load_path', default='tagging_models/tagging/rl_finetune/network_save_model', type=str, help='rl_finetune_network_load_path')
 
     parser.add_argument('--treebank', type=str, default='ctb', help='tree bank', choices=['ctb', 'ptb'])  # ctb
 
